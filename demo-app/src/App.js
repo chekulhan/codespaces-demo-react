e184cb72005296ec02274b1f3b5727224a077513
Max Programming <51731966+max-programming@users.noreply.github.com>
2020-09-20 02:18:04 +0000

    Created a React App using CRA

---
+++ demo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h3>I am a React app developed in Github codespaces</h3>
+    </div>
+  );
+}
+
+export default App;
